1417bf4fb2fa93f637da45e1b8e10593ea9a0394
tkgeorgexani <xanigeorge32@gmail.com>
2022-06-03 14:06:27 +0200

    alphabet printing

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -18,10 +18,10 @@ int main(void)
 char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
+putchar(letter, '\n');
 
 for (letter = 'A'; letter <= 'Z'; letter++)
-putchar(letter);
+putchar(letter, '\n');
 
 return (0);
 }
